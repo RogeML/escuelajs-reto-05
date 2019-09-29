d1d8ef1a07bbb4bd18442e7bf5d3842895c4b4cc
Rogelio Medina <rogelio.medina.ml@gmail.com>
2019-09-29 10:36:56 -0500

    Todos lo retos de la sesión 5.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
+localStorage.clear()
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
+      console.log(response)
+      const next = response.info.next
+      localStorage.setItem('next_fetch', next);
+
       const characters = response.results;
       let output = characters.map(character => {
         return `
@@ -23,8 +28,23 @@ const getData = api => {
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  try {
+    const nextFetchValue = localStorage.getItem('next_fetch');
+  if(nextFetchValue === null){
+    return await getData(API);
+  }else{
+    if (nextFetchValue === ""){
+      alert("ya no hay personajes!")
+      intersectionObserver.unobserve($observe)
+      
+    }
+    return await getData(nextFetchValue);
+  }
+  } catch (error) {
+    console.log(error)
+  }
+  
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
